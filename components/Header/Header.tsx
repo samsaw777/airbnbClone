1f6749082c6fb70b2f70a01760a06f2a9e09c863
Sameep <sameepsawant10@gmail.com>
2022-03-03 13:25:37 +0530

    user added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -40,7 +40,38 @@ const Header = () => {
           />
         </div>
         {/* User Side */}
-        <div className="">Lleft</div>
+        <div className="flex items-center">
+          <div className="flex p-1 border-2 border-gray-400 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 text-gray-400"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
       {/* Search Side */}
       <div
