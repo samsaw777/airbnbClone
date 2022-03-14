6b25dea67d24a9655a2cf7e2dea29955af6f7d3f
Sameep <sameepsawant10@gmail.com>
2022-03-14 20:47:39 +0530

    navbar for mobile fixed

--- components/Home/Header/MobileHeader.tsx
+++ components/Home/Header/MobileHeader.tsx
@@ -29,10 +29,13 @@ const MobileHeader = ({ placeHolder }: Props) => {
   });
   return (
     <>
+      {/* <div className="block sm:hidden"> */}
       <header
         className={classnames(
-          navbar || searchedLocation ? "bg-white shadow-xl" : "bg-[#051529]",
-          "sticky top-0 z-50 p-5 md:px-20"
+          navbar || searchedLocation
+            ? "bg-white shadow-xl sticky"
+            : "bg-[#051529] ",
+          "block sm:hidden w-full top-0 z-50 p-5 md:px-20"
         )}
       >
         <div
@@ -65,8 +68,35 @@ const MobileHeader = ({ placeHolder }: Props) => {
           </svg>
         </div>
       </header>
-      <div className="fixed w-full bottom-0 p-5 z-50 bg-white">
-        This is the bottom
+      {/* </div> */}
+      <div className="block sm:hidden">
+        <div
+          className={classnames(
+            navbar ? "fixed" : "hidden",
+            "flex justify-around w-full bottom-0 p-5 z-50 bg-gray-100 shadow-xl"
+          )}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7 text-red-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7 text-red-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
       </div>
       {searchedLocation && (
         <DatePicker
